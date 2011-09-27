bf6aa65089a119499fdf7186cfea0c903783cea6
Mikay <imikay@gmail.com>
2011-09-27 14:19:09 +0800

    Added the bookmarket ListAllLinkslet.js

---
+++ ListAllLinkslet.js
@@ -0,0 +1,9 @@
+(function()
+{
+  var listAllLinks=document.createElement('script');
+  
+  listAllLinks.type='text/javascript';
+  listAllLinks.src='http://imikay.github.com/Get-All-Links/js/ListAllLinks.js';
+  
+  document.getElementsByTagName('head')[0].appendChild(listAllLinks);
+})();
